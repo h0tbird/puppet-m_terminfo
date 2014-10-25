a2f56c9dfd7314ddd67feb6038fb1afa71e96f5c
Marc Villacorta <marc.villacorta@gmail.com>
2014-10-25 21:03:33 +0200

    Purge terminfo database

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,11 +6,6 @@ class terminfo (
 
 ) {
 
-  # Validate parameters:
-  validate_bool($purge)
-  validate_bool($backup)
-
-  # Manage the terminfo database:
   file { '/usr/share/terminfo':
     ensure  => directory,
     source  => "puppet:///modules/${module_name}/terminfo",
